7f10f6512ec4fda6adf78e5858bfd7650feededa
hd-david <mwewahoby@gmailcom>
2022-03-17 10:24:53 +0000

    first commit

---
+++ application.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+
+import csv
+
+
+
+Presidents = []
+with open("presidents.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        Presidents.append(row)
+     #   print(Presidents)
+
+
+app = Flask(__name__)
+
+# the function is filtering presidents based on the name selected
+# inputs: a list of dictionarie and str
+# outputs: dictionary
+
+@app.route("/information/<lastname>")
+def hello_world(lastname):
+    for President in Presidents:
+        if President["lastname"]== lastname:
+            return jsonify(President)
+    return "not found", 404
